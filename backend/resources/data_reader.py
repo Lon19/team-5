ac56846b7dc419638db685c8c91e221e664cd4f9
Robert Szafarczyk <robertszafa@gmail.com>
2019-11-01 23:37:41 +0000

    Merge with Rahul's.

--- backend/resources/data_reader.py
+++ backend/resources/data_reader.py
@@ -1,4 +1,28 @@
 import pandas as pd
+from pykml import parser
+
+
+kml_file = 'Wards_December_2016_Full_Clipped_Boundaries_in_the_UK.kml'
+root = parser.fromstring(open(kml_file, 'r').read().encode('utf-8'))
+folder = root.Document.Folder
+names = list()
+center_long = list()
+center_lat = list()
+coods = list()
+for p in folder.Placemark:
+    names.append(p.ExtendedData.SchemaData.SimpleData[2])
+    center_long.append(p.ExtendedData.SchemaData.SimpleData[8])
+    center_lat.append(p.ExtendedData.SchemaData.SimpleData[9])
+    coordinates = ""
+    try:
+        for poly in p.MultiGeometry.Polygon:
+            coordinates += poly.outerBoundaryIs.LinearRing.coordinates
+        coods.append(coordinates)
+    except:
+        for poly in p.Polygon:
+            coordinates += poly.outerBoundaryIs.LinearRing.coordinates
+        coods.append(coordinates)
+        continue
 
 df = pd.read_csv('claimant-count-all-wards.csv')
 
