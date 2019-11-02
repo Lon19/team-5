5ead884cc0c7449172218b1fa973d193013314bc
Harkrishen Matta <zdac105@live.rhul.ac.uk>
2019-11-02 11:44:15 +0000

    Final frontend

--- final-frontend/src/components/CompanyGoogleMap.vue
+++ final-frontend/src/components/CompanyGoogleMap.vue
@@ -1,8 +1,7 @@
 <template>
     <div>
-
         <v-container grid-list-md text-xs-center>
-            <v-layout row wrap>
+            <v-layout>
                 <v-flex xs6>
                     <v-text-field
                             class="compact-form"
@@ -21,62 +20,65 @@
                     <v-text-field
                             class="compact-form"
                             v-model="unemployemntRate"
-                            label="Unemployment Rate >"
+                            label="Unemployment Number"
                     ></v-text-field>
                 </v-flex>
                 <v-flex xs6>
                     <v-text-field
                             class="compact-form"
                             v-model="limit"
-                            label="Limit Highest >"
+                            label="Top Unemployment Wards"
                     ></v-text-field>
                 </v-flex>
-                <p>Gender</p>
-                <v-radio-group v-model="radios" :mandatory="false">
-                    <v-radio label="Male" value="Male"></v-radio>
-                    <v-radio label="Female" value="Female"></v-radio>
-                    <v-radio label="Total" value="Total"></v-radio>
-                </v-radio-group>
+
             </v-layout>
         </v-container>
-
-
-
 
         <v-row style="justify-content: space-evenly">
             <v-btn large center color="primary"
                    v-on:click="getWardsByPostcode(postcode1)">Postcode
             </v-btn>
-
             <v-btn large center color="primary"
                    v-on:click="getByWardName(wardName)">Ward Name
             </v-btn>
             <v-btn large center color="primary"
-                   v-on:click="getByUnemploymentRate(unemployemntRate)">Uneployment Rate
+                   v-on:click="getByUnemploymentRate(unemployemntRate)">Unemployment Number
             </v-btn>
             <v-btn large center color="primary"
                    v-on:click="getHighest(limit)">Get Highest
             </v-btn>
         </v-row>
 
+        <v-row style="justify-content: space-evenly">
+            <v-radio-group row label="Gender:" v-model="radios" :mandatory="false" >
+                <v-radio label="Male" value="Male"></v-radio>
+                <v-radio label="Female" value="Female"></v-radio>
+                <v-radio label="Total" value="Total"></v-radio>
+            </v-radio-group>
+        </v-row>
 
-
-
-
-        <gmap-map
-                :center="center"
-                :zoom="10"
-                style="width:100%;  height: 800px;"
-        >
-            <gmap-polygon
-                    v-for="(pin, index) in paths"
-                    :key=index
-                    :label=pin.name
-                    :paths=pin.position
-                    :options=pin.options
+        <v-row style="justify-content: space-evenly">
+            <div>
+                <img src="../assets/image.png">
+            </div>
+            <gmap-map
+                    :center="center"
+                    :zoom="10"
+                    style="width:80%;  height: 700px;"
             >
-            </gmap-polygon>
-        </gmap-map>
+                <gmap-polygon
+                        v-for="(pin, index) in paths"
+                        :key=index
+                        :label=pin.name
+                        :paths=pin.position
+                        :options=pin.options
+                >
+                </gmap-polygon>
+            </gmap-map>
+
+
+        </v-row>
+
     </div>
 </template>
 
@@ -105,8 +107,8 @@
                 axios
                     .get('http://127.0.0.1:5000/GetCoordinates', {
                         headers: {
-                            postCode: postcode.replace(/\s/g,''),
-                            nearests: 500
+                            postCode: postcode.replace(/\s/g, ''),
+                            nearests: 50
                         }
                     })
                     .then(response => {
@@ -128,7 +130,7 @@
 
                     });
             },
-            clear(){
+            clear() {
                 this.paths = [];
             },
             getByWardName(wardName) {
@@ -157,7 +159,7 @@
                 axios
                     .get('http://127.0.0.1:5000/GetWardsUnemployment', {
                         headers: {
-                            unemployment_number: un.replace(/\s/g,''),
+                            unemployment_number: un.replace(/\s/g, ''),
                             gender: this.radios
                         }
                     })
@@ -185,7 +187,7 @@
                 axios
                     .get('http://127.0.0.1:5000/GetHighest', {
                         headers: {
-                            limit: limit.replace(/\s/g,''),
+                            limit: limit.replace(/\s/g, ''),
                         }
                     })
                     .then(response => {
@@ -227,20 +229,16 @@
             },
             decideColour(num) {
                 if (num < 50) {
-                    return {fillColor: '#94949f', fillOpacity: 0.5}
+                    return {fillColor: '#03bd19', fillOpacity: 0.5}
                 } else if (num > 50 && num < 100) {
-                    return {fillColor: '#01e900', fillOpacity: 0.5}
-                }
-                else if (num > 100 && num < 150) {
                     return {fillColor: '#168200', fillOpacity: 0.5}
-                }
-                else if (num > 150 && num < 200) {
+                } else if (num > 100 && num < 150) {
                     return {fillColor: '#e8e900', fillOpacity: 0.5}
-                }
-                else if (num > 200 && num < 300) {
-                    return {fillColor: '#e97600', fillOpacity: 0.5}
-                }
-                else {
+                } else if (num > 150 && num < 200) {
+                    return {fillColor: '#f39433', fillOpacity: 0.5}
+                } else if (num > 200 && num < 300) {
+                    return {fillColor: '#a65900', fillOpacity: 0.5}
+                } else {
                     return {fillColor: '#c20400', fillOpacity: 0.5}
                 }
             },
