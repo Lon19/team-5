47493c4304b8f34312c59e66605ad0b86ca4b205
Harkrishen Matta <zdac105@live.rhul.ac.uk>
2019-11-02 03:29:17 +0000

    Final frontend vuetify application pushed

---
+++ final-frontend/src/App.vue
@@ -0,0 +1,86 @@
+<!--
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
+</template>
+-->
+
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+    <v-app>
+
+        <v-app-bar
+                color="deep-purple accent-4"
+                dark
+        >
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+            <v-toolbar-title>Page title</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+
+            <v-menu
+                    left
+                    bottom
+            >
+                <template v-slot:activator="{ on }">
+                    <v-btn icon v-on="on">
+                        <v-icon>mdi-dots-vertical</v-icon>
+                    </v-btn>
+                </template>
+
+                <v-list>
+                    <v-list-item
+                            v-for="n in 5"
+                            :key="n"
+                            @click="() => {}"
+                    >
+                        <v-list-item-title>Option {{ n }}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+        </v-app-bar>
+        <v-content>
+            <HelloWorld/>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+    import HelloWorld from './components/HelloWorld';
+
+    export default {
+        name: 'App',
+        components: {
+            HelloWorld,
+        },
+        data: () => ({
+            //
+        }),
+    };
+</script>
